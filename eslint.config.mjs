41b91b5a7d29964db5ec32dc6e5f73a759387b33
Parham-F <parham.parvizfarzam@gmail.com>
2025-10-25 01:19:42 -0600

    Exercise 10.2: setting up the ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,42 @@
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import babelParser from "@babel/eslint-parser";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const compat = new FlatCompat({ baseDirectory: __dirname });
+
+export default [
+  js.configs.recommended,
+
+  ...compat.config({
+    plugins: ["react", "react-native"],
+    settings: { react: { version: "detect" } },
+    extends: ["plugin:react/recommended"],
+    parser: "@babel/eslint-parser",
+    parserOptions: {
+      requireConfigFile: false,
+    },
+    env: { "react-native/react-native": true },
+    rules: {
+      "react/prop-types": "off",
+      "react/react-in-jsx-scope": "off",
+    },
+  }),
+
+  {
+    files: ["**/*.{js,jsx}"],
+    languageOptions: {
+      parser: babelParser,
+      parserOptions: {
+        requireConfigFile: false,
+        ecmaVersion: "latest",
+        sourceType: "module",
+        babelOptions: {
+          presets: ["@babel/preset-react"],
+        },
+      },
+    },
+  },
+];
